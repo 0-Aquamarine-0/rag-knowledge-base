b9e7bf411fca790b5604341d17d4a3a21aef4c6c
0-Aquamarine-0 <1513718049@qq.com>
2026-04-13 21:45:38 +0800

    Initial commit

---
+++ src/main/java/com/sera/ragknowledgebase/RagKnowledgeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.sera.ragknowledgebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagKnowledgeBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagKnowledgeBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sera/ragknowledgebase/RagKnowledgeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sera.ragknowledgebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagKnowledgeBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
